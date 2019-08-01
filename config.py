1a63d855907e14eef994960f83acd374445f15f0
scarletKX <kx993508907@gmail.com>
2019-08-01 20:28:37 +0800

    change config.py back with my api key

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    CONSUMER_KEY = os.environ.get('CONSUMER_KEY') or 'YOUR TWITTER DEVELOPER CONSUMER KEY'
-    CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET') or 'TOUR TWITTER DEVELOPER CONSUMER SECRET'
+    CONSUMER_KEY = os.environ.get('CONSUMER_KEY') or 'kd8Mq1XlxRnjMH4xdWxReBXKm'
+    CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET') or 'oBmGcMYkkYC4Cs45Bam3Uf0qTSaBi7KnC7DPAKKDIJMP6VgOVi'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
